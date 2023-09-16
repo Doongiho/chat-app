51338fd9136fd1e3b040b23049da507543219030
seojiho <happyluck0426@gamil.com>
2023-09-17 08:00:04 +0900

    RegisterPage 컴포넌트 생성 및 css

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,48 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function ReigistePage() {
+function RegisterPage() {
   return (
-    <div className="auth-wrapper">
+    <div className="auth-wapper">
       <div style={{textAlign:'center'}}>
         <h3>Register</h3>
       </div>
-       <form>
-       <label>Email</label>
-      <input
-         name="email"
-         type="email"
-        // {...register("exampleRequired", { required: true, maxLength: 10 })}
-      />
-      {/* {errors.exampleRequired && <p>This field is required</p>} */}
-      <label>Name</label>
-      <input
-       nmae ="name"
-        // {...register("exampleRequired", { required: true, maxLength: 10 })}
-      />
-      {/* {errors.exampleRequired && <p>This field is required</p>} */}
-      <label>Password</label>
-      <input
-        name='password'
-        type='password'
-        // {...register("exampleRequired", { required: true, maxLength: 10 })}
-      />
-      {/* {errors.exampleRequired && <p>This field is required</p>} */}
-      <label>Password Confirm</label>
-      <input
-        name='password_confirm'
-        type='password'
-        // {...register("exampleRequired", { required: true, maxLength: 10 })}
-      />
-      {/* {errors.exampleRequired && <p>This field is required</p>} */}
-      
-       <input type="submit" />
-       <Link style={{color:'gray', textDecoration:'none'}} to="login">
-         로그인하기
-       </Link>
-    </form>
+      <form >
+        <label>Email</label>
+        <input name="email" type='email' defaultValue="test"/>
+        <label>Name</label>
+        <input name="name"/>
+        <label>Password</label>
+        <input name="password" type='password'/>
+        <label>Password Confirm</label>
+        <input name="password conmfirm" type='password'/>
+        <input type="submit" />
+        <Link style={{color:'gray',textDecoration:'none', fontSize:'12px'}} to="login" > 이미 아이디가 있다면...</Link>
+      </form>
     </div>
   )
 }
 
-export default ReigistePage
+export default RegisterPage
+
